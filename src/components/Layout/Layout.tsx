000e7597fde913c5de52561d1ac1fdc22d5d735a
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-04-15 18:57:50 +0700

    add global layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { FC, ReactNode } from 'react'
+
+interface Props {
+  children?: ReactNode
+}
+
+const Layout: FC<Props> = ({ children }): JSX.Element => {
+  return (
+    <div
+      className={`min-h-full bg-white debug-screen ${process.env.NODE_ENV === 'development' ? 'debug-screens' : ''} `}
+    >
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default Layout
