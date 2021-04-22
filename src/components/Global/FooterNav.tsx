789cc69a494ef07eb687d991763fc2ef52cb569c
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-04-22 21:44:25 +0700

    fixed desktop bottom

--- src/components/Global/FooterNav.tsx
+++ src/components/Global/FooterNav.tsx
@@ -11,7 +11,7 @@ const FooterNav: FC<Props> = ({ lightTheme = true, prev, next, className }): JSX
   const navigate = useNavigate()
 
   return (
-    <nav className={`bottom-0 flex justify-center w-full space-x-4 mb-8 ${className}`}>
+    <nav className={`bottom-0 flex justify-center w-full space-x-4 pb-8 ${className}`}>
       {prev ? (
         <button
           type="button"
