55dd4e7be104a156f449e3109fe58be1b81d4457
Papon Sirimai <83017429+PaponSirimai@users.noreply.github.com>
2021-04-23 19:18:42 +0700

    Update SevereLevel.tsx

--- src/components/Intro/Infographic/SevereLevel.tsx
+++ src/components/Intro/Infographic/SevereLevel.tsx
@@ -3,16 +3,16 @@ import React, { FC } from 'react'
 const SevereLevel: FC = () => {
   return (
     <div className="flex flex-col space-y-3 font-light font-pridi md:text-lg">
-      <p className="text-white">โดยทั่วไป ภาวะสมองเสื่อมมักมีการดำเนิน ไปของโรคในระยะต่างๆ ดังนี้</p>
+      <p className="text-white">โดยทั่วไป ภาวะสมองเสื่อมมักมีการดำเนินไปของโรคในระยะต่างๆ ดังนี้</p>
       <h3 className="text-lg text-[#6866e7] bg-white py-1 px-4 rounded-md w-full">ภาวะความทรงจำบกพร่อง (MCI)</h3>
-      <p className="text-white">ใช้ชีวิตประจำวันได้ปกติ ดูแลตัวเองได้ แต่เริ่มหลงลืมมากขึ้น ตัดสินใจเรื่องต่างๆช้าลง</p>
+      <p className="text-white">ใช้ชีวิตประจำวันได้ปกติ ดูแลตัวเองได้ แต่เริ่มหลงลืมมากขึ้น ตัดสินใจเรื่องต่างๆ ช้าลง</p>
       <h3 className="text-lg text-[#9e473c] bg-[#f5e8e9] py-1 px-4 rounded-md w-full">ภาวะสมองเสื่อมเบื้องต้น</h3>
       <p className="text-white">
         อาการหลงลืมเริ่มมีผลต่อการใช้ชีวิตประจำวัน เช่นหลงทางบ่อยขึ้น ทำงานที่เคยทำมาตลอดไม่ได้
       </p>
       <h3 className="text-lg text-white bg-[#cd786a] py-1 px-4 rounded-md w-full">ภาวะสมองเสื่อมระดับปานกลาง</h3>
       <p className="text-white">
-        เลิกสนใจสิ่งที่เคยชอบเริ่มทำกิจวัตรประจำวันไม่ได้ เช่นเปิดทีวี อาบน้ำแต่งตัว ต้องมีคนคอยช่วยเหลือ
+        เลิกสนใจสิ่งที่เคยชอบ เริ่มทำกิจวัตรประจำวันไม่ได้ เช่นเปิดทีวี อาบน้ำแต่งตัว ต้องมีคนคอยช่วยเหลือ
       </p>
       <h3 className="text-lg text-white bg-[#9e473c] py-1 px-4 rounded-md w-full">ภาวะสมองเสื่อมขั้นรุนแรง</h3>
       <p className="text-white">
