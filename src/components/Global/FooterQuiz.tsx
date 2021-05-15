2f374c35fb405ab1ee0df1fdbae77fbcf9fc1b83
Chukkrit Visitsaktavorn <jukbot@yellotalk.co>
2021-05-15 21:55:04 +0700

    fixed issue 53

--- src/components/Global/FooterQuiz.tsx
+++ src/components/Global/FooterQuiz.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from 'react'
-import { Cross, Check } from '../Icons'
 interface Props {
   className?: string
   setAnswer: (answer: boolean) => void
@@ -8,14 +7,18 @@ interface Props {
 const FooterQuiz: FC<Props> = ({ className, setAnswer = () => {} }): JSX.Element => {
   return (
     <nav className={`bottom-0 flex justify-center w-full mb-12 space-x-6 mt-auto ${className}`}>
-      <Check
+      <div
         onClick={() => setAnswer(true)}
-        className="w-24 h-24 rounded-full shadow-dark bg-[#C9C8F3] cursor-pointer"
-      />
-      <Cross
+        className="w-24 h-24 rounded-full shadow-dark bg-[#C9C8F3] text-[#4842E0] cursor-pointer flex items-center justify-center text-3xl"
+      >
+        จริง
+      </div>
+      <div
         onClick={() => setAnswer(false)}
-        className="w-24 h-24 rounded-full shadow-dark bg-[#C9C8F3] cursor-pointer"
-      />
+        className="w-24 h-24 rounded-full shadow-dark bg-[#F1CACB] text-[#9E1E00] cursor-pointer flex items-center justify-center text-3xl"
+      >
+        เท็จ
+      </div>
     </nav>
   )
 }
